e32aed349f2f17478a440fc31c6e5072e825dd31
fumiko kosaka <fkosaka@netprotections.co.jp>
2016-06-22 05:45:19 +0000

    filmnow

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,35 +7,35 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
  
-  def create
-    @message = Message.new(message_params)
-    if @message.save
-     redirect_to root_path , notice: 'メッセージを保存しました'
-    else
+  #def create
+    #@message = Message.new(message_params)
+    #if @message.save
+     #redirect_to root_path , notice: 'メッセージを保存しました'
+    #else
       # メッセージが保存できなかった時#
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
-    end
-  end
+      #@messages = Message.all
+      #flash.now[:alert] = "メッセージの保存に失敗しました。"
+      #render 'index'
+    #end
+  #end
 
-  def edit
-  end
+  #def edit
+  #end
   
-  def update
-    if @message.update(message_params)
+  #def update
+    #if @message.update(message_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
-    else
+      #redirect_to root_path , notice: 'メッセージを編集しました'
+    #else
       # 保存に失敗した場合は編集画面へ戻す
-      render 'edit'
-    end
-  end
+      #render 'edit'
+    #end
+  #end
   
-  def destroy
-    @message.destroy
-    redirect_to root_path, notice: 'メッセージを削除しました'
-  end
+  #def destroy
+    #@message.destroy
+    #redirect_to root_path, notice: 'メッセージを削除しました'
+  #end
 
   private
   def message_params
